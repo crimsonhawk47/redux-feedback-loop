1ee3ee52083c6c75066548dc8514f699bf4e82ed
Gabriel Hawk <gabrielhawk@Gabriels-MacBook-Pro.local>
2020-01-24 17:02:25 -0600

    Add First Route and Dummy Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Link, Route } from 'react-router-dom'
 import './App.css';
+
+//Importing Components
+import Feeling from '../Feeling/Feeling'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+          <Route path="/feel">
+            <Feeling />
+          </Route>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Feeling/Feeling.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class Feeling extends Component{
+ 
+    render(){
+        return(
+        <div>
+            Hi I'm Feeling
+        </div>
+        )
+    
+    }
+}
+export default Feeling
