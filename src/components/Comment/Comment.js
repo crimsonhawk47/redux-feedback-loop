4c74458e260994078e48431cc24bf5039b6091b5
Gabriel Hawk <gabrielhawk@Gabriels-MacBook-Pro.local>
2020-01-25 12:02:41 -0600

    Fixed comment component

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class Comment extends Component{
+ 
+    render(){
+        return(
+        <div>
+            Hi I'm Comment
+        </div>
+        )
+    
+    }
+}
+export default Comment
