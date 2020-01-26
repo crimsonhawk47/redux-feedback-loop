df23a2e6208ecf6b9886559d14e92a30ba85bc8c
Gabriel Hawk <gabrielhawk@Martys-MBP.domain>
2020-01-26 12:57:30 -0600

    Post Route Completed

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {axios} from 'axios'
-import {connect} from 'react-redux'
-class Review extends Component{
+import React, { Component } from 'react';
+import axios from 'axios'
+import { connect } from 'react-redux'
+class Review extends Component {
 
-    submitForm = ()=>{
+    submitForm = () => {
         let feedback = this.props.reduxStore.feedback;
         let feedbackToSend = {
             feeling: feedback.feeling,
@@ -11,31 +11,35 @@ class Review extends Component{
             support: feedback.support,
             comment: feedback.comment
         }
-        console.log(`POSTING OBJECT`);
-        console.log(feedbackToSend);
-        
-        
-        
+        axios.post('/feedback', feedbackToSend)
+            .then((result) => {
+                console.log(result);
+                
+            }).catch((err)=>{
+                console.log(err);
+                
+            })
+
 
     }
- 
-    render(){
-        return(
-        <div>
-            <p>Feelings: {this.props.reduxStore.feedback.feeling}</p>
-            <p>Understanding: {this.props.reduxStore.feedback.understanding}</p>
-            <p>Support: {this.props.reduxStore.feedback.support}</p>
-            <p>Comments: {this.props.reduxStore.feedback.comment}</p>
-            <button onClick={this.submitForm}>Submit?</button>
-        </div>
+
+    render() {
+        return (
+            <div>
+                <p>Feelings: {this.props.reduxStore.feedback.feeling}</p>
+                <p>Understanding: {this.props.reduxStore.feedback.understanding}</p>
+                <p>Support: {this.props.reduxStore.feedback.support}</p>
+                <p>Comments: {this.props.reduxStore.feedback.comment}</p>
+                <button onClick={this.submitForm}>Submit?</button>
+            </div>
         )
-    
+
     }
 }
 
-const mapStateToProps = (reduxStore) =>{
-    return(
-        {reduxStore}
+const mapStateToProps = (reduxStore) => {
+    return (
+        { reduxStore }
     )
 }
 
